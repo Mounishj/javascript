8d2537ab7b623a8108e44ad0ce2e9b1a84ccc973
Mounishj <mounishjagani4@gmail.com>
2023-08-25 21:34:55 +0530

    Declaring variables
    Understand the declaring of variables
    Use let for declaring the variables that may change their values
    Use const for hardcoded values like birthdays

---
+++ javascript.js
@@ -0,0 +1,39 @@
+console.log("How are you doing today");
+console.log("How are you doing today");
+/*
+
+let message='Hello, World!'
+
+let intro;
+//JavaScript use camelCase
+
+intro= message
+alert(message);
+
+let $ = 2;
+let _ = 2;
+alert($+_);
+
+// To declare a constant (unchanging) variable, use const instead of let
+// Variables declared using const are called “constants”. They cannot be reassigned. An attempt to do so would cause an error:
+// In other words, capital-named constants are only used as aliases for “hard-coded” values.
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#FF7F00";
+
+let color = COLOR_BLUE;
+alert(color);
+
+// A variable name should have a clean, obvious meaning, describing the data that it stores.
+*/
+
+let admin;
+let name$;
+
+name$ = "John";
+admin = name$;
+alert(admin);
+
+let planet_earth;
+let user_Visiting_name;
